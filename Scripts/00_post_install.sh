f6bc49f3cfd3672ab226ec5f278e84cce596008a
jradtke-suse <james.radtke@suse.com>
2025-11-24 13:16:01 -0500

    generic update

--- Scripts/00_post_install.sh
+++ Scripts/00_post_install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Setup sudo for mansible (for later)
-echo 'mansible ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/mansible-nopasswd-all
-
 # Set the hostname of the host
 echo "observability-01" | sudo tee /etc/hostname
 echo "observability-02" | sudo tee /etc/hostname
 echo "observability-03" | sudo tee /etc/hostname
 
 # Set the primary interface of the host to a static value
+# TODO
+case platform in
+  sl-micro)
 sudo nmcli con mod 'Wired connection 1' ipv4.method manual ipv4.addresses 10.10.12.181/22 ipv4.gateway 10.10.12.1 ipv4.dns 10.10.12.11,10.10.12.10,8.8.8.8 connection.autoconnect yes
 sudo nmcli con mod 'Wired connection 1' ipv4.method manual ipv4.addresses 10.10.12.182/22 ipv4.gateway 10.10.12.1 ipv4.dns 10.10.12.11,10.10.12.10,8.8.8.8 connection.autoconnect yes
 sudo nmcli con mod 'Wired connection 1' ipv4.method manual ipv4.addresses 10.10.12.183/22 ipv4.gateway 10.10.12.1 ipv4.dns 10.10.12.11,10.10.12.10,8.8.8.8 connection.autoconnect yes
+  ;;
+  sles)
+  ;;
+esac
 
 # disable IPv6 (doesn't work in my setup)
 cat << EOF | tee /etc/sysctl.d/10-disable_ipv6.conf
